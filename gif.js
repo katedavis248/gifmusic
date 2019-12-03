8fc78ec24c7f33d83494ec8cf4b4ae2442bb3101
Kate Davis <katedavis248@yahoo.com>
2019-12-02 22:24:07 -0600

    loops

--- gif.js
+++ gif.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var topics = ['cadillac', 'audi', 'jaguar', 'lamborghini', 'ferrari', 'rolls royce', 'mustang', 'dodge-challenger'];
+    var topics = ['the talking heads', 'blondie', 'the beach boys', 'marvin gaye', 'black sabbath', 'devendra banhart'];
 
     // ========================================================
 
@@ -20,4 +20,33 @@ $(document).ready(function () {
     $(document).on('click', '.expression', function () {
 
         var car = $(this).html();
-        console.log(car);
+        console.log(bands);
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10";
+        // console.log(queryURL);
+        $.ajax({ url: queryURL, method: 'GET' })
+            .done(function (response) {
+                // grabs the data
+                var results = response.data;
+                // console.log(results);
+                //empties the div before adding more gifs
+                $('#expressView').empty();
+                //loops through the data
+                for (var j = 0; j < results.length; j++) {
+                    var imageDiv = $('<div>');
+                    var imageView = results[j].images.fixed_height.url;
+                    var still = results[j].images.fixed_height_still.url;
+                    // console.log(imageView);  
+                    var expressImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
+                    expressImage.attr('data-state', 'still');
+                    $('#expressView').prepend(expressImage);
+                    expressImage.on('click', playGif);
+
+                    // pulling the rating
+                    var rating = results[j].rating;
+                    // console.log(rating);
+                    var displayRated = $('<p>').text("Rating: " + rating);
+                    $('#expressView').prepend(displayRated);
+
+                } //for loop
+            }); // done response

2a5bb2414cb391b2d65eb8a109c1d6867d118865
Kate Davis <katedavis248@yahoo.com>
2019-12-02 22:21:20 -0600

    console logs

--- gif.js
+++ gif.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    var topics = ['the talking heads', 'blondie', 'marvin gaye', 'neil young', 'johnny cash', 'black sabbath'];
+    var topics = ['cadillac', 'audi', 'jaguar', 'lamborghini', 'ferrari', 'rolls royce', 'mustang', 'dodge-challenger'];
 
+    // ========================================================
 
-
-
+    //  create topics array buttons
     function buttonExpress() {
         $('#buttonsView').empty();
 
@@ -17,72 +17,7 @@ $(document).ready(function () {
         }
     }
     buttonExpress();
-
-
-    //on click
     $(document).on('click', '.expression', function () {
 
         var car = $(this).html();
         console.log(car);
-
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-        $.ajax({ url: queryURL, method: 'GET' })
-            .done(function (response) {
-
-                var results = response.data;
-
-                $('#expressView').empty();
-
-                for (var j = 0; j < results.length; j++) {
-                    var imageDiv = $('<div>');
-                    var imageView = results[j].images.fixed_height.url;
-                    var still = results[j].images.fixed_height_still.url;
-
-                    var expressImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
-                    expressImage.attr('data-state', 'still');
-                    $('#expressView').prepend(expressImage);
-                    expressImage.on('click', playGif);
-
-
-
-                }
-            });
-
-        function playGif() {
-            var state = $(this).attr('data-state');
-            console.log(state);
-            if (state == 'still') {
-                $(this).attr('src', $(this).data('animate'));
-                $(this).attr('data-state', 'animate');
-            } else {
-                $(this).attr('src', $(this).data('still'));
-                $(this).attr('data-state', 'still');
-            }
-
-        }
-
-    })
-
-
-
-
-    //adding new button
-    $(document).on('click', '#addExpress', function () {
-        if ($('#express-input').val().trim() == '') {
-            alert('please add car');
-        }
-        else {
-            var car = $('#express-input').val().trim();
-            topics.push(car);
-            $('#express-input').val('');
-            buttonExpress();
-            return false;
-
-        }
-
-    });
-
-
-
-}); 
